44c16620b1a74d6c96661a4056a8672555a2239c
Mohammed Miah <Mohamme61167@Mastek.Com>
2019-07-17 09:53:18 +0100

    first commit

---
+++ src/main/java/com/mastek/training/prem/prem/PremApplication.java
@@ -0,0 +1,13 @@
+package com.mastek.training.prem.prem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastek/training/prem/prem/PremApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mastek.training.prem.prem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PremApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("y7O");
+	}
+
+}
